1a94c39ed95d88eec15734374a2fccab0549e8b1
xiaoli <757199545@qq.com>
2020-05-04 10:58:00 +0800

    修改的信息

--- My_Test/src/HelloWorld.java
+++ My_Test/src/HelloWorld.java
@@ -7,6 +7,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int i = 0;
+        i+=10;
+        System.out.println(i);
     }
 
 }
