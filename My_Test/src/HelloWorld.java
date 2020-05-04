c55bfbb9eea530e6a6ba88aee96afba2e78b3081
xiaoli <757199545@qq.com>
2020-05-04 10:31:13 +0800

    测试idea提交git

---
+++ My_Test/src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    private Integer age; //年龄
+    private Integer 阿哥; //哈哈
+    private Integer sex; //sex
+    private Integer ss; //ss
+    private String name; //name
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
